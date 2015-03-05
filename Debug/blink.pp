f01b2c83a3886034520ff24646bd2ae08effcd4d
GamingX <gamingx2005@gmail.com>
2015-03-05 13:22:32 -0600

    Initial Commit for Lab01_B

---
+++ Debug/blink.pp
@@ -0,0 +1,13 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: /home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430.h
+blink.obj: /home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430f5529.h
+blink.obj: /home/syed/CCS/ccsv5/ccs_base/msp430/include/in430.h
+blink.obj: /home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+
+../blink.c: 
+/home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430.h: 
+/home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430f5529.h: 
+/home/syed/CCS/ccsv5/ccs_base/msp430/include/in430.h: 
+/home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
